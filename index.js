f5f19f05388f26e957402cbbc8856bf90f4546ff
wandera7 <julius.wandera@student.moringaschool.com>
2022-06-20 00:30:22 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+class Polygon{
+    constructor (numbers=[]){
+        this.numbers=numbers
+    }
+    get countSides(){
+        return this.numbers.length;
+    }
+    get perimeter(){
+        let perim=0
+      for(let i=0;i<this.numbers.length;i++){
+        perim+=this.numbers[i]
+
+      }
+      return perim
+    }
+}
+
+class Triangle extends Polygon{
+  get isValid(){
+    if (this.numbers[0] + this.numbers[1]<= this.numbers[2] || this.numbers[0] + this.numbers[2] <= this.numbers[1] || this.numbers[1] + this.numbers[2] <= this.numbers[0])
+    {
+        return false;
+    }
+    else{
+        return true;
+    }
+  }
+
+    
+}
+let shape =new Triangle([2,34,5,78,9,9])
+console.log(shape.isValid)
+
+class Square extends Polygon{
+    get isValid(){
+        let first = this.numbers[0];
+       for (let i=1; i<this.numbers.length; i++)
+           if (this.numbers[i] !== first){
+               return false;
+           }
+           else{
+            return true
+           }
+    
+    }
+    get area(){
+        return this.numbers[0]*this.numbers[1]
+    }
+}
